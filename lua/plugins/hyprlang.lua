1405cac1f54ec21286a8df6b987a3ad6cdfd4034
nya <191276298+Nyamogus@users.noreply.github.com>
2025-07-14 14:13:03 +0200

    fixed hyprlang and waybar-jsonc syntax highlighting

--- lua/plugins/hyprlang.lua
+++ lua/plugins/hyprlang.lua
@@ -2,7 +2,8 @@ return {
 	vim.filetype.add({
 		extension = { rasi = "rasi" },
 		pattern = {
-			[".*/waybar/config"] = "jsonc",
+			[".*/waybar/.*"] = "jsonc",
+			[".*/waybar/style/.*"] = "css",
 			[".*/mako/config"] = "dosini",
 			[".*/kitty/*.conf"] = "bash",
 			[".*/hypr/.*%.conf"] = "hyprlang",
