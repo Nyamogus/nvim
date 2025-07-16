93b587eacafa4abceb0d4f8b0bd28326e5011940
nya <191276298+Nyamogus@users.noreply.github.com>
2025-07-16 14:43:11 +0200

    fixed Mason menu not showing up

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,19 +1,19 @@
 return {
 	{
 		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"clangd",
-				"lua_ls",
-				"rust_analyzer",
-			},
-		},
-		config = function() end,
-		require("mason").setup({
-			registries = {
-				"github:mason-org/mason-registry",
-				"github:Crashdummyy/mason-registry",
-			},
-		}),
+		config = function()
+			require("mason").setup({
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry",
+				},
+				ensure_installed = {
+					"clangd",
+					"lua_ls",
+					"roslyn",
+					"rust_analyzer",
+				},
+			})
+		end,
 	},
 }
